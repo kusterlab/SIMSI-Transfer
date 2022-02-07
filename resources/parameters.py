fc1c2a5d13e0c6764caa7efadda7b67ea1c6cfa2
Matthew The <matthew.the@tum.de>
2022-02-07 18:39:57 +0100

    Clean up unused files

--- resources/parameters.py
+++
@@ -1,5 +0,0 @@
-reporter_columns = ['Reporter intensity 1', 'Reporter intensity 2', 'Reporter intensity 3', 'Reporter intensity 4',
-                    'Reporter intensity 5', 'Reporter intensity 6', 'Reporter intensity 7', 'Reporter intensity 8',
-                    'Reporter intensity 9', 'Reporter intensity 10', 'Reporter intensity 11', 'Reporter intensity 12',
-                    'Reporter intensity 13', 'Reporter intensity 14', 'Reporter intensity 15', 'Reporter intensity 16']
-
