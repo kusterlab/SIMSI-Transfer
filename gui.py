a5a5c8ca228726a5bdbeda9b5b9db11f2e3f0b90
Matthew The <matthew.the@tum.de>
2022-02-08 18:11:18 +0100

    Added output folder selection box

--- gui.py
+++ gui.py
@@ -30,6 +30,7 @@ class MainWindow(QtWidgets.QWidget):
         
         self._add_mq_txt_dir_field(layout)
         self._add_raw_dir_field(layout)
+        self._add_output_dir_field(layout)
         self._add_extra_params_field(layout)
         self._add_run_button(layout)
         self._add_log_textarea(layout)
@@ -77,6 +78,26 @@ class MainWindow(QtWidgets.QWidget):
         self.raw_dir_widget.setLayout(self.raw_dir_hbox_layout)
         
         layout.addRow(self.raw_dir_label, self.raw_dir_widget)
+    
+    def _add_output_dir_field(self, layout):
+        # fasta file input
+        self.output_dir_label = QtWidgets.QLabel("Select output folder")
+        #self.output_dir_label.setMargin(10)
+        
+        self.output_dir_widget = QtWidgets.QWidget()
+        
+        self.output_dir_hbox_layout = QtWidgets.QHBoxLayout()
+        self.output_dir_hbox_layout.setContentsMargins(0, 0, 0, 0)
+        
+        self.output_dir_line_edit = QtWidgets.QLineEdit()
+        self.output_dir_browse_button = QtWidgets.QPushButton("Browse")
+        self.output_dir_browse_button.clicked.connect(self.get_output_dir)
+        self.output_dir_hbox_layout.addWidget(self.output_dir_line_edit, stretch = 1)
+        self.output_dir_hbox_layout.addWidget(self.output_dir_browse_button)
+        
+        self.output_dir_widget.setLayout(self.output_dir_hbox_layout)
+        
+        layout.addRow(self.output_dir_label, self.output_dir_widget)
 
     def _add_extra_params_field(self, layout):
         # additional parameters input, TODO: make user friendly options for each important parameter
@@ -104,12 +125,16 @@ class MainWindow(QtWidgets.QWidget):
         layout.addRow(self.log_text_edit)
         
     def get_mq_txt_dir(self):
-        mq_txt_dir = QtWidgets.QFileDialog.getExistingDirectory(self, 'Open MaxQuant combined/txt folder' , '', QtWidgets.QFileDialog.ShowDirsOnly)
+        mq_txt_dir = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select MaxQuant combined/txt folder' , '', QtWidgets.QFileDialog.ShowDirsOnly)
         self.mq_txt_dir_line_edit.setText(mq_txt_dir)
     
     def get_raw_dir(self):
-        raw_dir = QtWidgets.QFileDialog.getExistingDirectory(self, 'Open folder with RAW files' , '', QtWidgets.QFileDialog.ShowDirsOnly)
+        raw_dir = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select folder with RAW files' , '', QtWidgets.QFileDialog.ShowDirsOnly)
         self.raw_dir_line_edit.setText(raw_dir)
+    
+    def get_output_dir(self):
+        output_dir = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select output folder' , '', QtWidgets.QFileDialog.ShowDirsOnly)
+        self.output_dir_line_edit.setText(output_dir)
     
     def process(self):
         start = time.time()
@@ -131,8 +156,9 @@ class MainWindow(QtWidgets.QWidget):
     def run_simsi_transfer(self):
         mq_txt_dir = self.mq_txt_dir_line_edit.text()
         raw_dir = self.raw_dir_line_edit.text()
+        output_dir = self.output_dir_line_edit.text()
         extra_params = self.args_line_edit.text()
-        simsi_transfer.main(['--mq_txt_folder', mq_txt_dir, '--raw_folder', raw_dir] + extra_params.split())
+        simsi_transfer.main(['--mq_txt_folder', mq_txt_dir, '--raw_folder', raw_dir, '--output_folder', output_dir] + extra_params.split())
         
 
 if __name__ == '__main__':
