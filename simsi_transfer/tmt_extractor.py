cfe4929aa1beba2295d0449c45308beb83ae5c6b
fhamood <firas.hamood@yahoo.de>
2022-03-03 02:17:29 +0100

    WIP: Implementing tmt reporter extraction; currently getting the associated ms2 spectra for the MS3 in mzml files is not working

--- simsi_transfer/tmt_extractor.py
+++ simsi_transfer/tmt_extractor.py
@@ -17,7 +17,7 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
 
-def get_correction_factors(mode: str):
+def get_correction_factors(mode: str, correction_factor_path: Path):
     if mode == 'topas':
         # TMT 11-plex Correction factors for LOT:
         # Reporter percentage   26   27N  27C  28N  28C  29N  29C  30N  30C  31N
@@ -85,28 +85,53 @@ def get_correction_factors(mode: str):
 
         # custom TMT correction factors: 10plex Krug et al
         # Reporter percentage   26   27N  27C  28N  28C  29N  29C  30N  30C  31N
-        correction = np.array([[100, 0.0, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 126 C Tag
-                               [0.0, 100, 0.0, 0.4, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0],  # 127 N Tag
-                               [4.8, 0.0, 100, 0.0, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0],  # 127 C Tag
-                               [0.0, 4.7, 0.0, 100, 0.0, 0.8, 0.0, 0.0, 0.0, 0.0],  # 128 N Tag
-                               [0.0, 0.0, 4.4, 0.0, 100, 0.0, 1.3, 0.0, 0.0, 0.0],  # 128 C Tag
-                               [0.0, 0.0, 0.0, 3.4, 0.0, 100, 0.0, 1.3, 0.0, 0.3],  # 129 N Tag
-                               [0.0, 0.0, 0.0, 0.0, 3.8, 0.0, 100, 0.0, 1.8, 0.0],  # 129 C Tag
-                               [0.0, 0.0, 0.0, 0.0, 0.0, 3.1, 0.0, 100, 0.0, 1.7],  # 130 N Tag
-                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.9, 0.0, 100, 0.0],  # 130 C Tag
-                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.8, 0.0, 100],  # 131 N Tag
-                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.1, 0.0],  # 131 C Overflow
-                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.7, 0.0, 1.6]  # 132 N Overflow
+        # correction = np.array([[100, 0.0, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 126 C Tag
+        #                        [0.0, 100, 0.0, 0.4, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0],  # 127 N Tag
+        #                        [4.8, 0.0, 100, 0.0, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0],  # 127 C Tag
+        #                        [0.0, 4.7, 0.0, 100, 0.0, 0.8, 0.0, 0.0, 0.0, 0.0],  # 128 N Tag
+        #                        [0.0, 0.0, 4.4, 0.0, 100, 0.0, 1.3, 0.0, 0.0, 0.0],  # 128 C Tag
+        #                        [0.0, 0.0, 0.0, 3.4, 0.0, 100, 0.0, 1.3, 0.0, 0.3],  # 129 N Tag
+        #                        [0.0, 0.0, 0.0, 0.0, 3.8, 0.0, 100, 0.0, 1.8, 0.0],  # 129 C Tag
+        #                        [0.0, 0.0, 0.0, 0.0, 0.0, 3.1, 0.0, 100, 0.0, 1.7],  # 130 N Tag
+        #                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.9, 0.0, 100, 0.0],  # 130 C Tag
+        #                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.8, 0.0, 100],  # 131 N Tag
+        #                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.1, 0.0],  # 131 C Overflow
+        #                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.7, 0.0, 1.6]  # 132 N Overflow
+        #                        ])
+
+        correction_dataframe = pd.read_csv(correction_factor_path, sep='\t')
+        correction = np.array([[100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 126 C Tag
+                               [0.0, 100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 127 N Tag
+                               [0.0, 0.0, 100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 127 C Tag
+                               [0.0, 0.0, 0.0, 100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 128 N Tag
+                               [0.0, 0.0, 0.0, 0.0, 100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 128 C Tag
+                               [0.0, 0.0, 0.0, 0.0, 0.0, 100, 0.0, 0.0, 0.0, 0.0, 0.0],  # 129 N Tag
+                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100, 0.0, 0.0, 0.0, 0.0],  # 129 C Tag
+                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100, 0.0, 0.0, 0.0],  # 130 N Tag
+                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100, 0.0, 0.0],  # 130 C Tag
+                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100, 0.0],  # 131 N Tag
+                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100],  # 131 C Tag
+                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 132 N Overflow
+                               [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]   # 132 C Overflow
                                ])
+
+        for i in range(11):
+            if i not in [0, 1, 2, 3]:
+                correction[i - 4, i] = correction_dataframe.iloc[i]['Correction factor -2 [%]']
+            if i not in [0, 1]:
+                correction[i - 2, i] = correction_dataframe.iloc[i]['Correction factor -1 [%]']
+            correction[i + 2, i] = correction_dataframe.iloc[i]['Correction factor +1 [%]']
+            if i not in [9, 10]:
+                correction[i + 4, i] = correction_dataframe.iloc[i]['Correction factor +2 [%]']
 
         # Theoretical TMT Masses in m/z
         TMT = np.array([126.127726, 127.124761, 127.131081, 128.128116, 128.134436, 129.131471,
-                        129.137790, 130.134825, 130.141145, 131.138180, 131.144499, 132.141535])
+                        129.137790, 130.134825, 130.141145, 131.138180, 131.144499, 132.141535, 132.147854])
 
         tmt_raw_col = ['raw_TMT1', 'raw_TMT2', 'raw_TMT3', 'raw_TMT4', 'raw_TMT5', 'raw_TMT6', 'raw_TMT7', 'raw_TMT8',
-                       'raw_TMT9', 'raw_TMT10', 'raw_TMT11', 'raw_TMT12']
+                       'raw_TMT9', 'raw_TMT10', 'raw_TMT11', 'raw_TMT12', 'raw_TMT13']
         tmt_corr_col = ['corr_TMT1', 'corr_TMT2', 'corr_TMT3', 'corr_TMT4', 'corr_TMT5', 'corr_TMT6', 'corr_TMT7',
-                        'corr_TMT8', 'corr_TMT9', 'corr_TMT10']
+                        'corr_TMT8', 'corr_TMT9', 'corr_TMT10', 'corr_TMT11']
 
     else:
         raise ValueError("Please select 'topas' or 'custom' mode.")
@@ -117,14 +142,14 @@ def get_correction_factors(mode: str):
     return TMT, tmt_raw_col, tmt_corr_col, correction_normalized
 
 
-def extract_tmt_reporters(mzml_files: List[Path], output_path: Path, num_threads: int = 1, mode: str = "custom", extraction_level: int = 2):
+def extract_tmt_reporters(mzml_files: List[Path], output_path: Path, correction_factor_path:Path, num_threads: int = 1, mode: str = "custom", extraction_level: int = 3):
     """
     Takes about 1.5 minute for a 700MB file with 40k MS2 scans
     """
     if not output_path.is_dir():
         output_path.mkdir(parents=True)
     
-    TMT, tmt_raw_col, tmt_corr_col, correction_normalized = get_correction_factors(mode)
+    TMT, tmt_raw_col, tmt_corr_col, correction_normalized = get_correction_factors(mode, correction_factor_path)
     
     tolerance = 6 * 1e-3 / 2
     tmt_upper = TMT + tolerance
@@ -144,7 +169,7 @@ def extract_tmt_reporters(mzml_files: List[Path], output_path: Path, num_threads
             for i, item in enumerate(reader):
                 if i % 1000 == 0:
                     logger.info(f"Processing spectrum {i}")
-                
+
                 if item['ms level'] != extraction_level:
                     continue
 
@@ -153,11 +178,13 @@ def extract_tmt_reporters(mzml_files: List[Path], output_path: Path, num_threads
                 if extraction_level == 2:
                     # scanseries['scanID'] = item['params']['scans']  # works for TOPAS data, adjust for other machines
                     # scanseries['scanID'] = re.search(r'scan=(\d+)"', item['params']['title'])[1]  # works for mgf extracted bailey
-                    scanseries['scanID'] = re.search(r'scan=(\d+)', item['id'])[1]  # works for mzml cohort3 set
+                    scanseries['scanID'] = re.search(r'scan=(\d+)', item['id'])[1]
                 else:
-                    for precursor in item['precursorList']['precursor']:
-                        if precursor['isolationWindow']['ms level'] == 2:
-                            scanseries['scanID'] = re.search(r'scan=(\d+)', precursor['spectrumRef'])[1]  # works for mzml cohort3 set
+                    # supposed to find parent MS2 spectrum for MS3 by looking into precursorList/precursor/spectrumRef
+                    scanseries['scanID'] = re.search(r'scan=(\d+)', item['precursorList']['precursor'][0]['spectrumRef'])[1]
+                    # for precursor in item['precursorList']['precursor']:
+                    #     scanseries['scanID'] = re.search(r'scan=(\d+)', precursor['spectrumRef'])[1]  # works for mzml cohort3 set
+                    #     continue
 
                 mz = np.array(item['m/z array'])
                 intensity = np.array(item['intensity array'])
