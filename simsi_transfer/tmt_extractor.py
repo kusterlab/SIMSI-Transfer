1d8abc278c7abcbdcc0c1011536b857d08338ef2
fhamood <firas.hamood@yahoo.de>
2022-04-06 15:58:44 +0200

    Multithreading TMT extraction; currently frying the CPU

--- simsi_transfer/tmt_extractor.py
+++ simsi_transfer/tmt_extractor.py
@@ -10,7 +10,6 @@ import pandas as pd
 import numpy as np
 from pyteomics import mzml
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -27,15 +26,15 @@ def get_correction_factors(correction_factor_path: Path):
                            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100, 0.0],  # 131 N Tag
                            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100],  # 131 C Tag
                            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # 132 N Overflow
-                           [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]   # 132 C Overflow
+                           [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]  # 132 C Overflow
                            ])
-    
+
     # Theoretical TMT Masses in m/z
     tmt_masses = np.array([126.127726, 127.124761, 127.131081, 128.128116, 128.134436, 129.131471,
-                    129.137790, 130.134825, 130.141145, 131.138180, 131.144499, 132.141535, 132.147854])
+                           129.137790, 130.134825, 130.141145, 131.138180, 131.144499, 132.141535, 132.147854])
 
-    tmt_raw_col = [f'raw_TMT{i}' for i in range(1,14)]
-    tmt_corr_col = [f'corr_TMT{i}' for i in range(1,12)]
+    tmt_raw_col = [f'raw_TMT{i}' for i in range(1, 14)]
+    tmt_corr_col = [f'corr_TMT{i}' for i in range(1, 12)]
 
     if correction_factor_path.is_file():
         correction_dataframe = pd.read_csv(correction_factor_path, sep='\t')
@@ -51,19 +50,35 @@ def get_correction_factors(correction_factor_path: Path):
 
     # Normalize correction factors
     correction_normalized = (correction / correction.sum(axis=0))
-    
+
     return tmt_masses, tmt_raw_col, tmt_corr_col, correction_normalized
 
 
-def extract_tmt_reporters(mzml_files: List[Path], output_path: Path, correction_factor_path:Path, num_threads: int = 1, extraction_level: int = 3):
+def extract_tmt_reporters(mzml_files: List[Path], output_path: Path, correction_factor_path: Path, num_threads: int = 1,
+                          extraction_level: int = 3):
     """
     Takes about 1.5 minute for a 700MB file with 40k MS2 scans
     """
     if not output_path.is_dir():
         output_path.mkdir(parents=True)
-    
+
+    if num_threads > 1:
+        from .utils.multiprocessing_pool import JobPool
+        processing_pool = JobPool(processes=num_threads)
+    for mzml_file in mzml_files:
+        if num_threads > 1:
+            processing_pool.applyAsync(extract_and_correct_reporters,
+                                       (mzml_file, output_path, correction_factor_path, extraction_level))
+        else:
+            extract_and_correct_reporters(mzml_file, output_path, correction_factor_path, extraction_level)
+
+    if num_threads > 1:
+        processing_pool.checkPool(printProgressEvery=1)
+
+
+def extract_and_correct_reporters(mzml_file, output_path, correction_factor_path, extraction_level):
     tmt_masses, tmt_raw_col, tmt_corr_col, correction_normalized = get_correction_factors(correction_factor_path)
-    
+
     tolerance = 6 * 1e-3 / 2
     tmt_upper = tmt_masses + tolerance
     tmt_lower = tmt_masses - tolerance
@@ -74,55 +89,53 @@ def extract_tmt_reporters(mzml_files: List[Path], output_path: Path, correction_
     convert_dict = {**convert_dict_raw, **convert_dict_corr, **convert_dict_other}
     dfcol = convert_dict.keys()
 
-    for mzml_file in mzml_files:
-        output_file = f'{output_path}/ext_{mzml_file.name}.txt'
-        if Path(output_file).is_file():
-            logger.info(f"Found extracted reporter ions at {output_file}, skipping extraction")
-            continue
-        
-        logger.info('Performing extraction for ' + mzml_file.name)
-        fileframe = pd.DataFrame(columns=dfcol)
+    output_file = f'{output_path}/ext_{mzml_file.name}.txt'
+    if Path(output_file).is_file():
+        logger.info(f"Found extracted reporter ions at {output_file}, skipping extraction")
+        return
 
-        with mzml.read(str(mzml_file)) as reader:
-            for i, item in enumerate(reader):
-                if i % 10000 == 0:
-                    logger.info(f"Processing spectrum {i}")
+    logger.info('Performing extraction for ' + mzml_file.name)
+    fileframe = pd.DataFrame(columns=dfcol)
 
-                if item['ms level'] != extraction_level:
-                    continue
+    with mzml.read(str(mzml_file)) as reader:
+        for i, item in enumerate(reader):
+            if i % 10000 == 0:
+                logger.info(f"Processing spectrum {i}")
 
-                scanseries = pd.Series(index=dfcol, dtype='float64')
+            if item['ms level'] != extraction_level:
+                continue
 
-                if extraction_level == 2:
-                    scanseries['scanID'] = re.search(r'scan=(\d+)', item['id'])[1]
-                else:
-                    # supposed to find parent MS2 spectrum for MS3 by looking into precursorList/precursor/spectrumRef
-                    scanseries['scanID'] = re.search(r'scan=(\d+)', item['precursorList']['precursor'][0]['spectrumRef'])[1]
+            scanseries = pd.Series(index=dfcol, dtype='float64')
 
-                mz = np.array(item['m/z array'])
-                intensity = np.array(item['intensity array'])
-                for c, (low, upp) in enumerate(zip(tmt_lower, tmt_upper)):
-                    start_idx = int(np.searchsorted(mz, low))
-                    end_idx = int(np.searchsorted(mz, upp))
-                    scanseries[f'raw_TMT{c + 1}'] = intensity[start_idx:end_idx].sum()
-                fileframe = pd.concat([fileframe, scanseries.to_frame().T], ignore_index=True)
-        fileframe['raw_file'] = mzml_file.name
+            if extraction_level == 2:
+                scanseries['scanID'] = re.search(r'scan=(\d+)', item['id'])[1]
+            else:
+                # supposed to find parent MS2 spectrum for MS3 by looking into precursorList/precursor/spectrumRef
+                scanseries['scanID'] = re.search(r'scan=(\d+)', item['precursorList']['precursor'][0]['spectrumRef'])[1]
 
-        fileframe = fileframe.astype(convert_dict)
+            mz = np.array(item['m/z array'])
+            intensity = np.array(item['intensity array'])
+            for c, (low, upp) in enumerate(zip(tmt_lower, tmt_upper)):
+                start_idx = int(np.searchsorted(mz, low))
+                end_idx = int(np.searchsorted(mz, upp))
+                scanseries[f'raw_TMT{c + 1}'] = intensity[start_idx:end_idx].sum()
+            fileframe = pd.concat([fileframe, scanseries.to_frame().T], ignore_index=True)
+    fileframe['raw_file'] = mzml_file.name
 
-        # TMT correction
-        logger.info('Extraction done, correcting TMT reporters for ' + mzml_file.name)
-        fileframe[tmt_corr_col] = pd.DataFrame(fileframe[tmt_raw_col].apply(
-            lambda tmt: np.linalg.lstsq(correction_normalized, tmt, rcond=None)[0].round(2), axis=1).tolist(),
-                                               columns=tmt_corr_col, index=fileframe[tmt_corr_col].index)
-        fileframe[tmt_corr_col] = fileframe[tmt_corr_col].where(fileframe[tmt_corr_col] > 10, 0)
-        fileframe.to_csv(output_file, sep='\t', index=False)
+    fileframe = fileframe.astype(convert_dict)
+
+    # TMT correction
+    logger.info('Extraction done, correcting TMT reporters for ' + mzml_file.name)
+    fileframe[tmt_corr_col] = pd.DataFrame(fileframe[tmt_raw_col].apply(
+        lambda tmt: np.linalg.lstsq(correction_normalized, tmt, rcond=None)[0].round(2), axis=1).tolist(),
+                                           columns=tmt_corr_col, index=fileframe[tmt_corr_col].index)
+    fileframe[tmt_corr_col] = fileframe[tmt_corr_col].where(fileframe[tmt_corr_col] > 10, 0)
+    fileframe.to_csv(output_file, sep='\t', index=False)
 
 
 if __name__ == "__main__":
-    input_files = Path(sys.argv[1])
-    extraction_level = int(sys.argv[2])
-    output_path = Path(sys.argv[3])
-    
-    extract_tmt_reporters([input_files], output_path, extraction_level=extraction_level)
-    
+    input_files_arg = Path(sys.argv[1])
+    extraction_level_arg = int(sys.argv[2])
+    output_path_arg = Path(sys.argv[3])
+
+    extract_tmt_reporters([input_files_arg], output_path_arg, extraction_level=extraction_level_arg)
