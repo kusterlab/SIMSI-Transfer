592689345edaa2e8323df6bb9ac9a82b09a035be
unknown <matthew.the@tum.de>
2022-04-28 19:09:48 +0200

    Moved tools for creating GUI to separate folder

--- build_gui.bat
+++
@@ -1,2 +0,0 @@
-:::: pyinstaller gui.py --noconfirm --onedir --name="SIMSI-Transfer"
-pyinstaller --upx-dir ../../Downloads/upx-3.96-win64/upx-3.96-win64 --noconfirm SIMSI-Transfer.spec && python create_gui_zip.py
---
+++ gui_tools/build_gui.bat
@@ -0,0 +1,2 @@
+:::: pyinstaller gui.py --noconfirm --onedir --name="SIMSI-Transfer"
+pyinstaller --upx-dir ../../../Downloads/upx-3.96-win64/upx-3.96-win64 --noconfirm ../SIMSI-Transfer.spec && python create_gui_zip.py
