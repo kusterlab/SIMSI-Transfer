830d6375dbd24f62d0559b8c6c19ae13a1794d82
Matthew The <matthew.the@tum.de>
2022-04-28 15:40:59 +0200

    Refactored transfer() function

--- simsi_transfer/transfer.py
+++ simsi_transfer/transfer.py
@@ -9,15 +9,15 @@ from .merging_functions import merge_with_msmsscanstxt, merge_with_msmstxt, merg
 
 logger = logging.getLogger(__name__)
 
+PHOSPHO_REGEX = re.compile(r'([STY])\(Phospho \(STY\)\)')
 
-def transfer(summary_df, rawseq='Sequence', modseq='Modified sequence', mask=False):
+
+def transfer(summary_df, mask=False):
     """
     Main function for transfers by clustering. Transfers identifications for merged dataframe and adds a column for
     identification type. Transferred columns are Sequence, Modified sequence, Proteins, Gene names, Protein Names,
     Charge, m/z, and Mass.
     :param summary_df: Summary dataframe, merged from cleaned msms.txt and MaRaCluster clusters.tsv file
-    :param rawseq: Column name of the raw sequence column
-    :param modseq: Column name of the modified sequence column
     :param mask: if false, uses "identification" column from summary frame. If set to a value, transfer uses the
     'identification_{mask}' column, needed for the masking analysis
     :return: DataFrame with transferred identifications resembling MaxQuant msmsScans.txt
@@ -26,46 +26,33 @@ def transfer(summary_df, rawseq='Sequence', modseq='Modified sequence', mask=Fal
         identification_column = f'identification_{mask}'
     else:
         identification_column = 'identification'
-        
-    # (mis)use the property that np.nan != np.nan to filter for identified scans
-    identified_scans = (summary_df[modseq] == summary_df[modseq])
-    unidentified_scans = (summary_df[modseq] != summary_df[modseq])
-    
-    # retain unique modified sequences per cluster
-    identified_clusters_df = summary_df[identified_scans].groupby('clusterID')[modseq].unique().reset_index(
-        name='cluster_modified_sequences')
 
-    identified_clusters_df[['representative_modified_sequence', 'representative_raw_sequence']] = identified_clusters_df[
-        'cluster_modified_sequences'].apply(get_modified_and_raw_sequence)
+    identified_scans = summary_df['Modified sequence'].notna()
 
-    # temporarily mark all scans in clusters with an identification as transferred ('t').
-    # The MQ identification will overwrite this column as direct ('d') a few lines below.
-    identified_clusters_df.loc[identified_clusters_df['representative_modified_sequence'].notna(), identification_column] = 't'
-    
-    summary_df = pd.merge(left=summary_df, right=identified_clusters_df, on=['clusterID'], how='left')
-    summary_df.loc[identified_scans, identification_column] = 'd'
-    summary_df.loc[unidentified_scans, modseq] = summary_df['representative_modified_sequence']
-    summary_df.loc[unidentified_scans, rawseq] = summary_df['representative_raw_sequence']
-    summary_df.drop(columns=['representative_modified_sequence', 'representative_raw_sequence'], axis=1, inplace=True)
-
-    agg_funcs = {'Modifications': get_main_object,
-                 'Proteins': get_main_object,
-                 'Gene Names': get_main_object,
-                 'Protein Names': get_main_object,
-                 'Charge': get_main_object,
+    agg_funcs = {'Sequence': get_unique_else_nan,
+                 'Modified sequence': get_consensus_modified_sequence,
+                 'Modifications': get_unique_else_nan,
+                 'Proteins': get_unique_else_nan,
+                 'Gene Names': get_unique_else_nan,
+                 'Protein Names': get_unique_else_nan,
+                 'Charge': get_unique_else_nan,
                  'm/z': 'mean',
                  'Mass': 'mean',
-                 'Missed cleavages': get_main_object,
-                 'Length': get_main_object,
-                 'Reverse': get_main_object}
+                 'Missed cleavages': get_unique_else_nan,
+                 'Length': get_unique_else_nan,
+                 'Reverse': get_unique_else_nan}
 
-    cluster_info_df = summary_df[identified_scans].groupby('clusterID', as_index=False)[list(agg_funcs.keys())].agg(agg_funcs)
-    cluster_info_df[identification_column] = 't'
+    cluster_info_df = summary_df[identified_scans].groupby('clusterID', as_index=False).agg(agg_funcs)
+    
+    # Mark all clusters with a unique identification as transferred ('t').
+    # Identifications by MQ will overwrite this column as direct identification ('d') a few lines below.
+    cluster_info_df[identification_column] = np.where(cluster_info_df['Modified sequence'].notna(), 't', None)
     
     replacement_dict = {k: "d_" + k for k in agg_funcs.keys()}
     cluster_info_df.rename(columns=replacement_dict, inplace=True)
     
-    summary_df = pd.merge(left=summary_df, right=cluster_info_df, on=['clusterID', identification_column], how='left')
+    summary_df = pd.merge(left=summary_df, right=cluster_info_df, on=['clusterID'], how='left')
+    summary_df.loc[identified_scans, identification_column] = 'd'
     summary_df.loc[
         summary_df[identification_column] == 't', replacement_dict.keys()] = summary_df.loc[
         summary_df[identification_column] == 't', replacement_dict.values()].to_numpy()
@@ -73,34 +60,34 @@ def transfer(summary_df, rawseq='Sequence', modseq='Modified sequence', mask=Fal
     return summary_df
 
 
-def get_modified_and_raw_sequence(sequence_array):
-    if len(sequence_array) == 0:
-        return pd.Series([np.NaN, np.NaN])
+def get_consensus_modified_sequence(sequences):
+    sequences = remove_nan_values(set(sequences))
+    if len(sequences) == 0:
+        return np.NaN
+    
+    if len(sequences) == 1:
+        return sequences[0]
 
-    raw_sequence = remove_modifications(sequence_array[0])
-    if len(sequence_array) == 1:
-        return pd.Series([sequence_array[0], raw_sequence])
-
-    sequence_array_lower = [re.sub(re.compile(r'([STY])\(Phospho \(STY\)\)'), lambda pat: pat.group(1).lower(), x) for x in sequence_array]
-    sequence_set_upper = {x.upper() for x in sequence_array_lower}
+    sequences_lower = [re.sub(PHOSPHO_REGEX, lambda pat: pat.group(1).lower(), x) for x in sequences]
+    sequence_set_upper = {x.upper() for x in sequences_lower}
     if len(sequence_set_upper) != 1:
-        return pd.Series([np.NaN, np.NaN])
+        return np.NaN
 
-    modified_sequence = generate_modified_sequence_annotation(sequence_array, sequence_array_lower)
-    return pd.Series([modified_sequence, raw_sequence])
+    modified_sequence = generate_modified_sequence_annotation(sequences, sequences_lower)
+    return modified_sequence
 
 
-def generate_modified_sequence_annotation(sequence_array, sequence_array_lower):
-    num_mods = len(re.findall(r'[sty]', sequence_array_lower[0]))
-    phospho_positions = {m.start() + 1 for x in sequence_array_lower for m in re.finditer(r'[sty]', x)}
+def generate_modified_sequence_annotation(sequences, sequences_lower):
+    num_mods = len(re.findall(r'[sty]', sequences_lower[0]))
+    phospho_positions = {m.start() + 1 for x in sequences_lower for m in re.finditer(r'[sty]', x)}
     phospho_positions = sorted(list(phospho_positions))
     phopsho_positions_string = "/".join(map(lambda x: f'p{x}', phospho_positions))
-    mod_sequence_without_phospho = remove_modifications(sequence_array[0], remove_phospho_only=True)
+    mod_sequence_without_phospho = remove_modifications(sequences[0], remove_phospho_only=True)
     return f"{mod_sequence_without_phospho}.{num_mods}.{phopsho_positions_string}"
 
 
 def remove_modifications(mod_sequence, remove_phospho_only=False):
-    raw_sequence = re.sub(re.compile(r'([STY])\(Phospho \(STY\)\)'), '', mod_sequence)
+    raw_sequence = re.sub(PHOSPHO_REGEX, '', mod_sequence)
     if remove_phospho_only:
         return raw_sequence
     raw_sequence = raw_sequence.replace('(Acetyl (Protein N-term))', '')
@@ -109,7 +96,7 @@ def remove_modifications(mod_sequence, remove_phospho_only=False):
     return raw_sequence
 
 
-def get_main_object(input_list):
+def get_unique_else_nan(input_list):
     """
     Returns nan if no unique sequence is found in inputlist, or the sequence if its unique
     :param input_list: List of peptide sequences
