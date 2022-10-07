ec6bf987e56339ede6d6e71d0ba22403397258ff
fhamood <firas.hamood@yahoo.de>
2022-10-07 17:49:40 +0200

    working on automatic exe builder

--- gui_tools/build_gui.bat
+++ gui_tools/build_gui.bat
@@ -1,2 +1,2 @@
 :::: pyinstaller gui.py --noconfirm --onedir --name="SIMSI-Transfer"
-pyinstaller --upx-dir C:\UPX\upx-3.96\ --noconfirm ..\SIMSI-Transfer.spec && python create_gui_zip.py
+pyinstaller --upx-dir ..\UPX\upx-3.96\ --noconfirm ..\SIMSI-Transfer.spec && python create_gui_zip.py
