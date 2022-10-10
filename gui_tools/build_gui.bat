4c06a3f596e75255fbde3e5097523559da7798ac
fhamood <74977668+fhamood@users.noreply.github.com>
2022-10-10 15:11:36 +0200

    Automatic exe build (#17)
    * started working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder; cleanup
    
    * working on automatic exe builder; cleanup
    
    * working on automatic exe builder; cleanup
    
    * working on automatic exe builder; cleanup
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * working on automatic exe builder
    
    * Fixing UPX download
    
    * Fixing UPX download
    
    * Fixing UPX download
    
    * Fixing UPX unzipping
    
    * Fixing UPX download
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Fixing batch script execution
    
    * Finalizing automatic exe building
    
    Co-authored-by: fhamood <firas.hamood@yahoo.de>

--- gui_tools/build_gui.bat
+++ gui_tools/build_gui.bat
@@ -1,2 +1,2 @@
 :::: pyinstaller gui.py --noconfirm --onedir --name="SIMSI-Transfer"
-pyinstaller --upx-dir ../../../Downloads/upx-3.96-win64/upx-3.96-win64 --noconfirm ../SIMSI-Transfer.spec && python create_gui_zip.py
+pyinstaller --upx-dir ..\UPX\upx-3.96\ --noconfirm ..\SIMSI-Transfer.spec && python create_gui_zip.py
